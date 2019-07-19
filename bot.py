68b1a4fbbb38deff4dccd72ee8321e55abfcc35c
Jack <babybishops@gmail.com>
2019-07-19 20:15:56 +0200

    React to peach emojis

--- bot.py
+++ bot.py
@@ -8,18 +8,30 @@ import butt
 class ButtBot(discord.Client):
 
     async def on_message(self, message):
+        if self.is_visible(message):
+            if self.should_buttify(message.clean_content):
+                response, buttified = butt.buttify(message.clean_content)
+                if buttified:
+                    await message.channel.send(response)
+
+    async def on_reaction_add(self, reaction, user):
+        message = reaction.message
+        emoji_code = ord(reaction.emoji)
+        PEACH = 127825
+        if emoji_code == PEACH:
+            response, buttified = butt.buttify(message.clean_content)
+            await message.channel.send(response)
+
+    def is_visible(self, message):
         server_id = message.guild.id
         channel_id = message.channel.id
         if not config.is_channel_authorized(server_id, channel_id):
-            return
+            return False
 
         if message.author == self.user:
-            return
+            return False
 
-        if self.should_buttify(message.clean_content):
-            response, buttified = butt.buttify(message.clean_content)
-            if buttified:
-                await message.channel.send(response)
+        return True
 
     def should_buttify(self, message):
         if config.contains_keyword(message):
