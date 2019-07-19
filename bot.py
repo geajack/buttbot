785afe3945ebf733a2a844093720abdb77737a77
Jack <babybishops@gmail.com>
2019-07-19 20:18:06 +0200

    React to emoji only

--- bot.py
+++ bot.py
@@ -7,13 +7,6 @@ import butt
 
 class ButtBot(discord.Client):
 
-    async def on_message(self, message):
-        if self.is_visible(message):
-            if self.should_buttify(message.clean_content):
-                response, buttified = butt.buttify(message.clean_content)
-                if buttified:
-                    await message.channel.send(response)
-
     async def on_reaction_add(self, reaction, user):
         message = reaction.message
         emoji_code = ord(reaction.emoji)
@@ -21,21 +14,3 @@ class ButtBot(discord.Client):
         if emoji_code == PEACH:
             response, buttified = butt.buttify(message.clean_content)
             await message.channel.send(response)
-
-    def is_visible(self, message):
-        server_id = message.guild.id
-        channel_id = message.channel.id
-        if not config.is_channel_authorized(server_id, channel_id):
-            return False
-
-        if message.author == self.user:
-            return False
-
-        return True
-
-    def should_buttify(self, message):
-        if config.contains_keyword(message):
-            return True
-
-        chance_to_butt = config.get_chance_to_butt()
-        return random() <= chance_to_butt
--- config.py
+++ config.py
@@ -6,24 +6,6 @@ def get_token():
     config = get_config()
     return config["token"]
 
-def is_channel_authorized(server_id, channel_id):
-    config = get_config()
-    servers = config.get("servers", [])
-    for server in servers:
-        if server_id == server["id"]:
-            channels = server.get("channels", [])
-            for channel in channels:
-                if channel_id == channel["id"]:
-                    return True
-    return False
-
-def get_chance_to_butt():
-    config = get_config()
-    return config.get("chance_to_butt", 0.5)
-
-def contains_keyword(message):
-    return "buttbot" in message.lower()
-
 def get_config():
     CONFIG_PATH = get_application_root() / "config.yaml"
     with open(CONFIG_PATH, "r") as config_file:
