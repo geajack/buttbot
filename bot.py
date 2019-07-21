b5bc2ca784561f1805a36a64147762394916fc68
Jack <babybishops@gmail.com>
2019-07-21 10:31:44 +0200

    Reply even to old messages

--- bot.py
+++ bot.py
@@ -7,10 +7,13 @@ import butt
 
 class ButtBot(discord.Client):
 
-    async def on_reaction_add(self, reaction, user):
-        message = reaction.message
-        emoji_code = ord(reaction.emoji)
+    async def on_raw_reaction_add(self, payload):
+        emoji_code = ord(payload.emoji.name)
+        channel_id = payload.channel_id
+        message_id = payload.message_id
+        channel = await self.fetch_channel(channel_id)
+        message = await channel.fetch_message(message_id)
         PEACH = 127825
         if emoji_code == PEACH:
             response, buttified = butt.buttify(message.clean_content)
-            await message.channel.send(response)
+            await channel.send(response)
